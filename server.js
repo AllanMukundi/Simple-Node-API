14667fb49e1db347df83aa3515737711ccd8451c
Allan M <allanmukundi@hotmail.com>
2017-06-06 14:51:13 -0400

    HTTP methods added.
    HTTP methods added.

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-// Call packages
+// Base setup
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var mongoose = require('mongoose');
+var Goose = require('./app/models/goose');
+
+mongoose.connect('mongodb://allan:test@ds113282.mlab.com:13282/goose-api');
 
 // Configure app to use bodyParser()
 app.use(bodyParser.urlencoded({extended: true}));
@@ -9,13 +13,84 @@ app.use(bodyParser.json());
 
 var port = 8000;
 
-// Setting up routes
+// Routes---------------------------------
 var router = express.Router();
+
+// Middleware example
+router.use(function(req, res, next) {
+  console.log("A request was sent.");
+  next();
+})
 
 router.get('/', function(req, res) {
   res.json({ message: 'API test.'});
 });
 
+router.route('/goose')
+
+  .post(function(req, res) {
+    var goose = new Goose();
+    goose.name = req.body.name;
+
+    goose.save(function(err) {
+      if(err) {
+        res.send(err);
+      }
+      res.json({ message: 'Goose created successfully.' });
+    });
+
+  })
+
+  .get(function(req, res) {
+    Goose.find(function(err, goose) {
+      if(err) {
+        res.send(err);
+      }
+      res.json(goose);
+    });
+  });
+
+  router.route('/goose/:goose_id')
+
+    .get(function(req, res) {
+      Goose.findById(req.params.goose_id, function(err, goose) {
+        if(err) {
+          res.send(err);
+        }
+        res.json(goose);
+      });
+    })
+
+    .put(function(req, res) {
+      Goose.findById(req.params.goose_id, function(err, goose) {
+        if(err) {
+          res.send(err);
+        }
+        goose.name = req.body.name
+
+        goose.save(function(err) {
+          if(err) {
+            res.send(err);
+          }
+          res.json({ message: 'Goose updated successfully.' });
+        });
+      });
+    })
+
+    .delete(function(req, res) {
+      Goose.remove({
+        _id: req.params.goose_id
+      }, function(err, goose) {
+        if(err) {
+          res.send(err);
+        }
+        res.json({ message: 'Goose deleted successfully.' });
+      });
+    });
+
+//----------------------------------------
+
+// Register routes
 app.use('/api', router);
 
 app.listen(port);
